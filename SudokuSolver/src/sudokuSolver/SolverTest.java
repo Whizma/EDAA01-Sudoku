a137888a39c2116e82c1295bf9025999f1324055
isak <is3006ma-s@student.lu.se>
2021-12-13 10:40:11 +0100

    testklass skapad

---
+++ SudokuSolver/src/sudokuSolver/SolverTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package sudokuSolver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author isak
+ *
+ */
+class SolverTest {
+
+
+}
