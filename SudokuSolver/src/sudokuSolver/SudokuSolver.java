651c0320976d69536aeb010c87632de1e0503e90
Erik Malmgren <er8540ma-s@student.lu.se>
2021-12-14 14:38:47 +0100

    Javadoc är fixat

--- SudokuSolver/src/sudokuSolver/SudokuSolver.java
+++ SudokuSolver/src/sudokuSolver/SudokuSolver.java
@@ -2,19 +2,15 @@ package sudokuSolver;
 
 public interface SudokuSolver {
 	/**
-	 * Sets the number nbr in box r, c.
+	 * Puts digit in the box row, col.
 	 * 
-	 * @param r
-	 *            The row
-	 * @param c
-	 *            The column
-	 * @param nbr
-	 *            The number to insert in box r, c
-	 * @throws IllegalArgumentException        
-	 *             if r or c is outside [0..getDimension()-1] or
-	 *             number is outside [1..9] 
+	 * @param r   The row
+	 * @param c   The column
+	 * @param digit The digit to insert in box row, col
+	 * @throws IllegalArgumentException if row, col or digit is outside the range
+	 *                                  [0..9]
 	 */
-	public void add(int r, int c, int nbr);
+	public void add(int r, int c, int digit);
 	
 	/**
 	 * Returns the number in box r,c. If the box i empty 0 is returned.
@@ -23,11 +19,9 @@ public interface SudokuSolver {
 	 *            The row
 	 * @param c
 	 *            The column
-	 * @param number
-	 *            The number to insert in r, c
 	 * @return the number in box r,c or 0 if the box is empty.
 	 * @throws IllegalArgumentException
-	 *             if r or c is outside [0..getDimension()-1]
+	 *             if r or c is outside [0..9]
 	 */
 	public int get(int r, int c);
 	
@@ -40,13 +34,14 @@ public interface SudokuSolver {
 	 * 
 	 * @throws
 	 * IllegalArgumentException 
-	 * 				if r or c is outside [0..getDimension()-1]
+	 * 				if r or c is outside [0..9]
 	 */
 	public void remove(int r, int c);
 	
 
 	/**
 	 * Checks that all filled in digits follows the the sudoku rules.
+	 * @return true if postions are valid
 	 */
 	public boolean isValid();
 		
@@ -62,7 +57,7 @@ public interface SudokuSolver {
 	public void clear();
 		
 	/**
-	 * Returns the numbers in the grid. An empty box i represented
+	 * Returns the numbers in the sudoku. An empty box i represented
 	 * by the value 0.
 	 * 
 	 * @return the numbers in the grid
